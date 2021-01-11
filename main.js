e6d915158fe4562584169c728f9e16ad7db2e53a
FRANCESCO <OLIVOTTI>
2021-01-11 16:51:11 +0100

    completed exercise

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
-	//Code
-});
+let app = new Vue ({
+
+
+    el: "#root",
+    
+    data: {
+
+        listaCD : [],
+
+    },
+    methods: {
+        
+    },
+    mounted() {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music").then(response => {
+            this.listaCD = response.data.response;
+            console.log(this.listaCD);
+        }).catch(error=>{
+            console.log(error);
+        })
+    },
+
+})
